546c0c7df487b6252866e0baf4f3843a2605c2ba
JMIKEPAD <mchparodi@gmail.com>
2022-01-28 13:23:46 +0100

    Array.reduce/map/filter

--- array.js
+++ array.js
@@ -1,293 +1,303 @@
-// let firstArray = [1, true, 'pippo'];
+// // let firstArray = [1, true, 'pippo'];
 
-// console.log(firstArray);
+// // console.log(firstArray);
 
-// let secondArray = ['pippo', 'pluto', 'paperino'];
+// // let secondArray = ['pippo', 'pluto', 'paperino'];
 
-// console.log(secondArray.length);
+// // console.log(secondArray.length);
 
-// for (let i = 0; i < secondArray.length; i++) {
-//     const element = secondArray[i];
-//     console.log(element)
-// }
+// // for (let i = 0; i < secondArray.length; i++) {
+// //     const element = secondArray[i];
+// //     console.log(element)
+// // }
 
-// for (const element of secondArray) {
-//     console.log(element);
+// // for (const element of secondArray) {
+// //     console.log(element);
 
-// }
-// // lifo
-// secondArray.push('topolino');
+// // }
+// // // lifo
+// // secondArray.push('topolino');
 
-// console.log(secondArray);
+// // console.log(secondArray);
 
-// let ultimo_elemento = secondArray.pop();
+// // let ultimo_elemento = secondArray.pop();
 
-// console.log(secondArray);
+// // console.log(secondArray);
 
-// console.log(ultimo_elemento);
-// //fifo
-// let primoElemento = secondArray.shift()
+// // console.log(ultimo_elemento);
+// // //fifo
+// // let primoElemento = secondArray.shift()
 
-// console.log(secondArray);
+// // console.log(secondArray);
 
-// console.log(primoElemento);
+// // console.log(primoElemento);
 
-// secondArray.unshift('paperone');
+// // secondArray.unshift('paperone');
 
-// console.log(secondArray);
+// // console.log(secondArray);
 
-// console.log(secondArray[1]);
+// // console.log(secondArray[1]);
 
-// secondArray[1] = 'paperina'
+// // secondArray[1] = 'paperina'
 
-// console.log(secondArray);
+// // console.log(secondArray);
 
-// let thirdArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // let thirdArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// let subArray = thirdArray.slice(2, 5);
+// // let subArray = thirdArray.slice(2, 5);
 
-// console.log(subArray);
+// // console.log(subArray);
 
-// console.log(thirdArray);
+// // console.log(thirdArray);
 
-// let subArray2 = thirdArray.splice(2, 3, 12, 13, 14);
+// // let subArray2 = thirdArray.splice(2, 3, 12, 13, 14);
 
-// console.log(subArray2);
+// // console.log(subArray2);
 
-// console.log(thirdArray);
+// // console.log(thirdArray);
 
-// let numeroPiuGrande = Math.max(5, 10, 2)
+// // let numeroPiuGrande = Math.max(5, 10, 2)
 
-// console.log(numeroPiuGrande);
+// // console.log(numeroPiuGrande);
 
-// let numeroPiuGrandeDellArray = Math.max(...thirdArray);
+// // let numeroPiuGrandeDellArray = Math.max(...thirdArray);
 
-// console.log("numero piu grande dell'array", numeroPiuGrandeDellArray);
+// // console.log("numero piu grande dell'array", numeroPiuGrandeDellArray);
 
-// function stringaPiuLunga(...stringhe) {
-//     let str = ""
-//     for (let i = 0; i < stringhe.length; i++) {
-//         const element2 = stringhe[i];
-//         if (str.length < element2.length) {
-//             str = element2;
-//         }
-//     }
-//     return str
-// }
+// // function stringaPiuLunga(...stringhe) {
+// //     let str = ""
+// //     for (let i = 0; i < stringhe.length; i++) {
+// //         const element2 = stringhe[i];
+// //         if (str.length < element2.length) {
+// //             str = element2;
+// //         }
+// //     }
+// //     return str
+// // }
 
-// console.log(stringaPiuLunga("pippo", "topolino", "pluto"));
+// // console.log(stringaPiuLunga("pippo", "topolino", "pluto"));
 
 
-// function max(...numbers) {
-//     let massimo = -Infinity;
+// // function max(...numbers) {
+// //     let massimo = -Infinity;
 
 
 
-//     for (let i = 0; i < numbers.length; i++) {
-//         const element = numbers[i];
+// //     for (let i = 0; i < numbers.length; i++) {
+// //         const element = numbers[i];
 
-//         if (massimo < element) {
-//             massimo = element;
-//         }
-//     }
-//     return massimo;
-// }
+// //         if (massimo < element) {
+// //             massimo = element;
+// //         }
+// //     }
+// //     return massimo;
+// // }
 
-// console.log(max(-100, -12, -3, -1001));
+// // console.log(max(-100, -12, -3, -1001));
 
-// function min(...numbers) {
-//     let minimo = +Infinity;
+// // function min(...numbers) {
+// //     let minimo = +Infinity;
 
 
 
-//     for (let i = 0; i < numbers.length; i++) {
-//         const element = numbers[i];
+// //     for (let i = 0; i < numbers.length; i++) {
+// //         const element = numbers[i];
 
-//         if (minimo > element) {
-//             minimo = element;
-//         }
-//     }
-//     return minimo;
-// }
+// //         if (minimo > element) {
+// //             minimo = element;
+// //         }
+// //     }
+// //     return minimo;
+// // }
 
-// console.log(min(-100, -12, -3, -1001)); //primo esercizio
+// // console.log(min(-100, -12, -3, -1001)); //primo esercizio
 
-// function creaArray(number) {
-//     let arrayVuoto = [];
-//     for (let i = 0; i <= number; i++) {
+// // function creaArray(number) {
+// //     let arrayVuoto = [];
+// //     for (let i = 0; i <= number; i++) {
 
-//         arrayVuoto.push(i);
+// //         arrayVuoto.push(i);
 
-//     }
+// //     }
 
-//     return arrayVuoto;
-// }
+// //     return arrayVuoto;
+// // }
 
-// let nuovoArray = creaArray(5);
+// // let nuovoArray = creaArray(5);
 
-// console.log(nuovoArray); // secondo esercizio
+// // console.log(nuovoArray); // secondo esercizio
 
-// let arrayDiStringhe = ["ancona","torino","genova",""]
-// function ArrayIniziali(arrayDiStringhe) {
+// // let arrayDiStringhe = ["ancona","torino","genova",""]
+// // function ArrayIniziali(arrayDiStringhe) {
 
-//     let arrayIniz = [];
+// //     let arrayIniz = [];
 
-//     for (let j = 0; j < arrayDiStringhe.length; j++) {
+// //     for (let j = 0; j < arrayDiStringhe.length; j++) {
 
-//         if (arrayDiStringhe[j] === "") {
-//             arrayDiStringhe.splice(j, 1);
-//         }
-//     }
+// //         if (arrayDiStringhe[j] === "") {
+// //             arrayDiStringhe.splice(j, 1);
+// //         }
+// //     }
 
-//     for (let i = 0; i < arrayDiStringhe.length; i++) {
-//         const element = arrayDiStringhe[i].toUpperCase();
+// //     for (let i = 0; i < arrayDiStringhe.length; i++) {
+// //         const element = arrayDiStringhe[i].toUpperCase();
 
-//         arrayIniz.push(element[0]);
-//     }
-// return arrayIniz;
-// }
-// let NuovoArray= ArrayIniziali(arrayDiStringhe);
-// console.log(NuovoArray);// terzo esercizio
+// //         arrayIniz.push(element[0]);
+// //     }
+// // return arrayIniz;
+// // }
+// // let NuovoArray= ArrayIniziali(arrayDiStringhe);
+// // console.log(NuovoArray);// terzo esercizio
 
 
-// primo esercizio di compito
-function range(start, end, step = 1) {
-
-    let array1 = []
-    let inizio = step > 0 ? start : end;
-
-    let fine;
-
-    if (step > 0) {
-        fine = end
-    } else {
-        fine = start
-    }
-    for (let i = start; i <= end; i += step) {
-
-        array1.push(i)
-    }
-
-    return array1
-
-}
-
-console.log(range(0, 10))
-
-function sum(...numbers) {
-
-    let result = numbers[0]
-
-    for (let i = 1; i < numbers.length; i++) {
-        // const element = numbers[i];
-        result += numbers[i];
-    }
-    return result;
-}
-
-console.log("somma", sum(...range(5)));
-
-
+// // primo esercizio di compito
 // function range(start, end, step = 1) {
 
-//     let somma = 0;
+//     let array1 = []
+//     let inizio = step > 0 ? start : end;
 
-//     for (let i = start; i <= end; i+= step) {
+//     let fine;
 
-//         somma = somma + i
+//     if (step > 0) {
+//         fine = end
+//     } else {
+//         fine = start
+//     }
+//     for (let i = start; i <= end; i += step) {
+
+//         array1.push(i)
 //     }
 
-//     console.log(somma);
+//     return array1
 
 // }
 
-// console.log(range(-1, 10))
+// console.log(range(0, 10))
 
-// secondo esercizio di compito
+// function sum(...numbers) {
 
-// let arrayoriginal = [1, 2, 3, 4, 5, 6];
+//     let result = numbers[0]
 
-// function arrayReverse(arrayoriginal) {
-
-//     let arrayReversed = [];
-
-//     // for (let i = arrayoriginal.length - 1; i >= 0; i--) {
-
-//     //     arrayReversed.push(arrayoriginal[i]);
-
-//     // }
-
-//     for (const number of arrayoriginal) {
-//         arrayReversed.unshift(number)
+//     for (let i = 1; i < numbers.length; i++) {
+//         // const element = numbers[i];
+//         result += numbers[i];
 //     }
-//     return arrayReversed;
+//     return result;
 // }
 
-// let reverseArray = arrayReverse(arrayoriginal);
+// console.log("somma", sum(...range(5)));
 
-// console.log(reverseArray);
 
-// function reverseInPlace(arrayoriginal) {
-//     // arrayoriginal= arrayReverse(arrayoriginal);
-//     for (let i = 0; i < arr.length / 2; i++) {
-//         const headelement = arr[i];
-//         const tailelement = arr[arr.length - 1 - i];
+// // function range(start, end, step = 1) {
 
-//         // let temp = headelement
-//         // headelement = tailelement
-//         // tailelement = temp   
-//         let temp = arr[i]
-//         arr[i]= arr[arr.length - 1 -i]
-//         arr[arr.length - 1 -i] = temp
-//     }
-// }
+// //     let somma = 0;
 
-// reverseInPlace(arrayoriginal)
+// //     for (let i = start; i <= end; i+= step) {
 
-// console.log("in place", arrayoriginal);
+// //         somma = somma + i
+// //     }
 
-let array5 = [7, 4, 100, 12];
+// //     console.log(somma);
 
-function doubleFirstElement(array) {
+// // }
+
+// // console.log(range(-1, 10))
+
+// // secondo esercizio di compito
+
+// // let arrayoriginal = [1, 2, 3, 4, 5, 6];
+
+// // function arrayReverse(arrayoriginal) {
+
+// //     let arrayReversed = [];
+
+// //     // for (let i = arrayoriginal.length - 1; i >= 0; i--) {
+
+// //     //     arrayReversed.push(arrayoriginal[i]);
+
+// //     // }
+
+// //     for (const number of arrayoriginal) {
+// //         arrayReversed.unshift(number)
+// //     }
+// //     return arrayReversed;
+// // }
+
+// // let reverseArray = arrayReverse(arrayoriginal);
+
+// // console.log(reverseArray);
+
+// // function reverseInPlace(arrayoriginal) {
+// //     // arrayoriginal= arrayReverse(arrayoriginal);
+// //     for (let i = 0; i < arr.length / 2; i++) {
+// //         const headelement = arr[i];
+// //         const tailelement = arr[arr.length - 1 - i];
+
+// //         // let temp = headelement
+// //         // headelement = tailelement
+// //         // tailelement = temp   
+// //         let temp = arr[i]
+// //         arr[i]= arr[arr.length - 1 -i]
+// //         arr[arr.length - 1 -i] = temp
+// //     }
+// // }
+
+// // reverseInPlace(arrayoriginal)
+
+// // console.log("in place", arrayoriginal);
+
+// let array5 = [7, 4, 100, 12];
+
+// function doubleFirstElement(array) {
     
-    // let tempArray = []
+//     // let tempArray = []
     
-    // for (let i = 0; i < array.length; i++) {
-    //     tempArray.push(array[i])
+//     // for (let i = 0; i < array.length; i++) {
+//     //     tempArray.push(array[i])
         
-    // }
-    let tempArray = [...array]
+//     // }
+//     let tempArray = [...array]
 
-    tempArray[0] = tempArray[0] * 2;
+//     tempArray[0] = tempArray[0] * 2;
 
-    return tempArray;
+//     return tempArray;
 
-}
-
-let newarray = doubleFirstElement(array5)
-
-console.log(array5);
-
-console.log(newarray);
-
-let finalArray = [23, 13, 5, 12];
-
-// function sumFirst4ElementsOfArray(array) {
-//     return array[0] + array[1] + array[2] + array[3]
 // }
 
-function sumFirst4ElementsOfArray([el0, el1, el2, el3]) {
-    return el0 + el1 + el2 + el3;
-}
+// let newarray = doubleFirstElement(array5)
 
-let lastElement = {value: 3, nextelement:null}
+// console.log(array5);
 
-let secondElement = {value: 7, nextelement:lastElement}
+// console.log(newarray);
 
-let firstElement = {value: 5, nextelement:secondElement}
+// let finalArray = [23, 13, 5, 12];
+
+// // function sumFirst4ElementsOfArray(array) {
+// //     return array[0] + array[1] + array[2] + array[3]
+// // }
+
+// function sumFirst4ElementsOfArray([el0, el1, el2, el3]) {
+//     return el0 + el1 + el2 + el3;
+// }
+
+// let lastElement = {value: 3, nextelement:null}
+
+// let secondElement = {value: 7, nextelement:lastElement}
+
+// let firstElement = {value: 5, nextelement:secondElement}
 
 
-console.log(firstElement.value, firstElement.nextelement.value, firstElement.nextelement.nextelement.value);
+// console.log(firstElement.value, firstElement.nextelement.value, firstElement.nextelement.nextelement.value);
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ arrayManipulation.js
@@ -0,0 +1,365 @@
+// let myArray=[100,101,9,1000,12,-3]
+
+// function filterToRemoveGreaterThan100(arrayToFilter) {
+//     let tempArray=[];
+//     for (const element of arrayToFilter) {
+//         if (element <= 100) {
+//             tempArray.push(element);
+//         }
+//     }
+//     return tempArray;
+// }
+
+// console.log( filterToRemoveGreaterThan100(myArray));
+
+
+// function greaterThan30(element) {
+//     // if (element > 30) {
+//     //     return true;
+//     // } else{
+//     //     return false;
+//     // }
+
+
+
+//     return (element > 30);
+// }
+
+// function isEven(element){
+//     if ( element %2 === 0 ){
+//     return true;
+//     } else{
+//         return false;
+//     }
+//     return (element % 2 === 0);
+// }
+
+// function isPositive(element) {
+//     return (element > 0);
+// }
+
+
+
+
+
+// function filter(arrayToFilter, filterFunction) {
+//     let tempArray=[];
+//     for (const element of arrayToFilter) {
+//         if (filterFunction(element)) {
+//             tempArray.push(element);
+//         }
+//     }
+//     return tempArray;
+// }
+
+
+
+
+
+// console.log("mio filter ",filter(myArray, isEven ));
+
+// console.log("is positive filter",myArray.filter(isPositive));
+
+
+// function isNegative(element, index) {
+//     return (element < 0);
+// }
+
+// let isNegative2 = function (element, index) {
+//     return (element < 0);
+// }
+
+// let isNegative3 = (element, index) => element < 0;
+
+// // let isNegative3 = (element) => {
+// //     return (element < 0);
+// // }
+
+
+
+
+// console.log(myArray.filter(isNegative));
+// console.log(myArray.filter(function(element){
+//     return (element<0);
+// }));
+
+
+
+
+
+// function hasEvenPosition(element, index) {
+//     if (index % 2 ===0) {
+//         return true;
+
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log( myArray.filter( hasEvenPosition ) );
+
+// console.log(myArray.filter(function(element,index){
+//     return index % 2 === 0;
+// }));
+
+// console.log( myArray.filter((e,i) => i % 2 === 0 ) );
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function addOneToArray(arrayToModify) {
+//     let tempArray = [];
+
+//     for (const element of arrayToModify) {
+//         let modifiedElement= element + 1;
+//         tempArray.push(modifiedElement);
+//     }
+//     return tempArray;
+// }
+
+// console.log(addOneToArray(myArray));
+
+
+// function map(arrayToModify,alterationFunction) {
+//     let tempArray = [];
+//     for (const element of arrayToModify) {
+//         let modifiedElement= alterationFunction(element);
+//         tempArray.push(modifiedElement);
+//     }
+//     return tempArray;
+// }
+
+// console.log(map(myArray, addOneToArray));
+
+// console.log(myArray.map(addOneToArray));
+
+// console.log(myArray.map(function (element) {
+//     return element + 1;
+// }));
+
+// console.log(myArray.map((element)=> element + 1));
+
+
+
+// function elementIndexProduct(element,index) {
+//     return element * index;
+// }
+
+// console.log(myArray.map(elementIndexProduct));
+
+// console.log(myArray.map(function (element,index) {
+//     return element * index;
+// }));
+
+// console.log(myArray.map((e,i)=> e * i));
+
+
+// console.log(myArray.map(elementIndexProduct).filter(isNegative));
+
+
+let testArray1 = [12, 57, 1001, -67, -12, 5, 27, 48, -6000];
+
+let testArray2 = ["pippo", "pluto", "paperino", "topolino", "paperone", "paperoga"];
+
+
+/// teniamo i numeri divisibili per 3 ;
+function filteredArray1(element) {
+    let tempArray = [];
+    for (const element of testArray1) {
+        if (element % 3 === 0) {
+            tempArray.push(element);
+        }
+    }
+    return tempArray;
+
+}
+let result = console.log(filteredArray1(testArray1));
+
+/// soluzione 2 
+
+console.log(testArray1.filter((e) => e % 3 === 0));
+
+
+// /// teniamo le stringhe che sono più lunghe di 6 caratteri ;
+function filteredArray2(element) {
+    let tempArray = [];
+    for (const element of testArray2) {
+        if (element.length > 6) {
+            tempArray.push(element);
+        }
+    }
+    return tempArray
+}
+console.log(filteredArray2(testArray2));
+
+/// soluzione 2 
+
+console.log(testArray2.filter((e) => e.length > 6));
+
+// /// tutti i numeri dovranno essere ridotti del 10%;
+function mappedArray1(element) {
+    let tempArray = [];
+    for (let element of testArray1) {
+        element = element - (element * 10) / 100
+        tempArray.push(element)
+    }
+    return tempArray;
+}
+console.log(mappedArray1(testArray1));
+
+/// soluzione 2
+
+console.log(testArray1.map((e) => e -= e * 10 / 100));
+
+// /// tutte le stringhe dovranno essere maiuscole;
+function mappedArray2(element) {
+    let tempArray = [];
+    for (let element of testArray2) {
+        element = element.toUpperCase();
+        tempArray.push(element);
+    }
+    return tempArray;
+}
+console.log(mappedArray2(testArray2));
+
+/// soluzione 2
+
+console.log(testArray2.map((e) => e.toUpperCase()));
+
+// /// tutti i numeri andranno traformati in positivi e andranno eliminati i numeri maggiori di 100;
+function modifiedArray1(element) {
+    let tempArray = [];
+    for (let element of testArray1) {
+        if (element < 0) {
+            element *= (-1);
+        }
+        if (element >= 0 && element < 100) {
+            tempArray.push(element);
+        }
+
+
+    }
+    return tempArray;
+}
+console.log(modifiedArray1(testArray1));
+
+/// soluzione 2 
+
+console.log(testArray1.map((e) => Math.abs(e)).filter((e => e <= 100)));
+
+// /// tutte le stringhe dovranno avere la prima lettera maiuscola e andranno eliminate le stringhe che non contengono la lettera r;
+
+function modifiedArray2(element) {
+    let tempArray = [];
+    for (const element of testArray2) {
+        if (element.includes = "r" || "R") {
+            tempArray.push(element[0].toUpperCase())
+        }
+    }
+    return tempArray;
+}
+console.log(modifiedArray2(testArray2));
+
+///// soluzione 2 
+
+console.log(testArray2.map((e) => e[0].toUpperCase() + e.slice(1)).filter((e) => e.toLowerCase().includes(e.slice(1))));
+
+function sumAllArray(arrayToSum) {
+    let result = 0;
+    for (const element of arrayToSum) {
+        result = result + element;
+    }
+    return result
+}
+
+console.log(sumAllArray(testArray1));
+
+function sum(first, second) {
+    return first + second;
+}
+
+function product(previous, current, index) {
+    return previous * current;
+}
+
+function sumEvenIndex(previous, current, index) {
+    if (index % 2 === 0) {
+        return previous +  current;
+    } else {
+        return previous
+    }
+}
+
+function reduce(arrayToAggregate, aggregationFunction, startingElement) {
+
+    let result;
+
+    let startingIndex;
+
+    if (startingElement !== undefined) {
+        startingIndex = 0
+        result = startingElement;
+    } else {
+        result = arrayToAggregate[0]
+        startingIndex = 1
+    }
+    for (let i = 0; i < arrayToAggregate.length; i++) {
+
+        const element = arrayToAggregate[i];
+
+        result = aggregationFunction(result, element, i);
+    }
+
+    return result
+}
+
+console.log(reduce(testArray1, sum));
+
+console.log(reduce(testArray1, product));
+
+console.log(reduce(testArray1, sum, 100));
+
+console.log(reduce(testArray1, sumEvenIndex));
+
+console.log(testArray1.reduce((previous, current, index, array) => previous + current,0));
+
+
+
+let testArray3 =["la","vergogna", "casa","dannazione", "ha", "pippo","preso", "secchio", "fuoco"];
+
+//console.log(testArray3.reduce((array,indice) => ));
+
+function sumEvenIndexStr(previous, current, index) {
+    if (index % 2 === 0) {
+        return previous + " " + current;
+    } else {
+        return previous
+    }
+}
+
+function reduce(arrayToAggregate, aggregationFunction, startingElement) {
+
+    let result;
+
+    let startingIndex;
+
+    if (startingElement !== undefined) {
+        
+        result = startingElement;
+    } else {
+        result = arrayToAggregate[0]
+        startingIndex = 1
+    }
+    for (let i = startingIndex; i < arrayToAggregate.length; i++) {
+    const element = arrayToAggregate[i];
+
+        result = aggregationFunction(result, element, i);
+
+        
+    }
+
+    return result
+}
+
+console.log(testArray3.reduce(sumEvenIndexStr));
+
+console.log(testArray3.reduce((previous, current, index, array) => index % 2 === 0 ? previous + " " + current: previous));
